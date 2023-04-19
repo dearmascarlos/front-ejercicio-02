bc254daf8516db725f94ec7c797bc4e32ec4e79c
Charlie <correo.carlosdearmas@gmail.com>
2023-04-19 15:53:18 +0100

    html structure

--- translator-app/pages/index.js
+++ translator-app/pages/index.js
@@ -2,7 +2,33 @@
 export default function Home() {
   return (
     <>
-    
+      <header>
+        <h1>Translate app</h1>
+      </header>
+      <body>
+        <div>
+          <h3>Enter English Text</h3>
+          <input placeholder="Enter your text here"></input>
+        </div>
+        
+        <div>
+          <h3>Select desired language</h3>
+          <button>Afrikaans</button>
+          <button>Arabic</button>
+          <button>French</button>
+          <button>Hindi</button>
+          <button>Japanese</button>
+          <button>Portuguese</button>
+          <button>Spanish</button>
+        </div>
+
+        <div>
+          <h3>Output</h3>
+          <div></div>
+        </div>
+      </body>
+
+
     </>
   )
 }
